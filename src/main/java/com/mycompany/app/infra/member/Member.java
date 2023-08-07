5870c77805b9851f436e330e644b8fcae955b32e
songseungkwan <ssk3553@Naver.com>
2023-08-07 16:50:07 +0900

    0807 팀프 (멤버)

---
+++ src/main/java/com/mycompany/app/infra/member/Member.java
@@ -0,0 +1,103 @@
+package com.mycompany.app.infra.member;
+
+public class Member {
+
+	private String seq;
+	private String name;
+	private String id;
+	private String password;
+	private String emailFull;
+	private String emailDomain;
+	private String emailAccount;
+	private String dob;
+	private String phone;
+	private String zipCode;
+	private String address;
+	private String addressDetail;	
+	private String delNy;
+	
+	
+
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmailFull() {
+		return emailFull;
+	}
+	public void setEmailFull(String emailFull) {
+		this.emailFull = emailFull;
+	}
+	public String getEmailDomain() {
+		return emailDomain;
+	}
+	public void setEmailDomain(String emailDomain) {
+		this.emailDomain = emailDomain;
+	}
+	public String getEmailAccount() {
+		return emailAccount;
+	}
+	public void setEmailAccount(String emailAccount) {
+		this.emailAccount = emailAccount;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddressDetail() {
+		return addressDetail;
+	}
+	public void setAddressDetail(String addressDetail) {
+		this.addressDetail = addressDetail;
+	}
+	public String getDelNy() {
+		return delNy;
+	}
+	public void setDelNy(String delNy) {
+		this.delNy = delNy;
+	}
+
+	
+	
+	
+}
