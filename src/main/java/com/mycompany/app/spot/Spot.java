fdf6404baece17d4a31aa40e76815cb095c1029b
songseungkwan <ssk3553@Naver.com>
2023-08-08 17:21:16 +0900

    0808 spot dto,mappewr,form,list 완성

---
+++ src/main/java/com/mycompany/app/spot/Spot.java
@@ -0,0 +1,104 @@
+package com.mycompany.app.spot;
+
+public class Spot {
+
+	private String seq;
+	private Integer type;
+	private String name;
+	private String zipCode;
+	private String address;
+	private String addressDetail;
+	private String sido;
+	private String tel;
+	private String openTime;
+	private String closedTime;
+	private String adultPrice;
+	private String childPrice;
+	private String detailText;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddressDetail() {
+		return addressDetail;
+	}
+	public void setAddressDetail(String addressDetail) {
+		this.addressDetail = addressDetail;
+	}
+	public String getSido() {
+		return sido;
+	}
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getOpenTime() {
+		return openTime;
+	}
+	public void setOpenTime(String openTime) {
+		this.openTime = openTime;
+	}
+	public String getClosedTime() {
+		return closedTime;
+	}
+	public void setClosedTime(String closedTime) {
+		this.closedTime = closedTime;
+	}
+	public String getAdultPrice() {
+		return adultPrice;
+	}
+	public void setAdultPrice(String adultPrice) {
+		this.adultPrice = adultPrice;
+	}
+	public String getChildPrice() {
+		return childPrice;
+	}
+	public void setChildPrice(String childPrice) {
+		this.childPrice = childPrice;
+	}
+	public String getDetailText() {
+		return detailText;
+	}
+	public void setDetailText(String detailText) {
+		this.detailText = detailText;
+	}
+
+	
+	
+	
+	
+	
+}
