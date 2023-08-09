16cf807a9cf23f1296059f4433e39e1e514bf2c9
songseungkwan <ssk3553@Naver.com>
2023-08-09 17:34:29 +0900

    0809 오후 2차

---
+++ src/main/java/com/mycompany/app/spot/SpotDao.java
@@ -0,0 +1,49 @@
+package com.mycompany.app.spot;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SpotDao {
+
+	@Inject
+	@Resource(name = "sqlSession")
+	private SqlSession sqlSession; // 변수명 선언
+	
+	private static String namespace = "com.mycompany.app.spot.SpotMapper";
+	
+//	paging
+	public int selectOneCount(SpotVo vo){ return sqlSession.selectOne(namespace + ".selectOneCount", vo); }
+	
+	public Spot selectOne(SpotVo vo) { return sqlSession.selectOne(namespace + ".selectOne", vo); }
+	
+	public List<Spot> selectList(SpotVo vo) {
+		return sqlSession.selectList(namespace + ".selectList", vo ); 
+		// selectList : 반환값 list, vo=가변 가능성 현저히 낮음.
+		}
+	
+	
+
+	
+	
+	
+	// selectList , selectOne과는 다르게 무조건 dto를 써야한다. vo 사용 불가능!!!
+	public int update(Spot dto) { return sqlSession.update(namespace + ".update", dto); }
+	
+	public int delete(Spot dto) { return sqlSession.delete(namespace + ".delete", dto); }
+
+	public int insertSpot(Spot dto) { return sqlSession.insert(namespace + ".insertSpot", dto); }
+	public int insertSpotInfo(Spot dto) { return sqlSession.insert(namespace + ".insertSpotInfo", dto); }
+	public int insertSpotCloseDay(Spot dto) { return sqlSession.insert(namespace + ".insertSpotCloseDay", dto); }
+
+	public int uelete(Spot dto) { return sqlSession.update(namespace + ".uelete", dto); }
+	
+	// 파일업로드
+	public int insertUploaded(Spot dto) { return sqlSession.insert(namespace + ".insertUploaded", dto); }
+	
+}
---
+++ src/main/java/com/mycompany/app/spot/SpotVo.java
@@ -0,0 +1,287 @@
+package com.mycompany.app.spot;
+
+public class SpotVo {
+
+	
+	private String seq;
+	private Integer type;
+	private String name;
+	private String zipCode;
+	private String address;
+	private String addressDetail;
+	private String sido;
+	private String tel;
+	private String openTime;
+	private String closedTime;
+	private String adultPrice;
+	private String childPrice;
+	private String detailText;
+	
+	
+	// paging
+		private int thisPage = 1;									// 현재 페이지
+//		private int rowNumToShow = Constants.ROW_NUM_TO_SHOW;		// 화면에 보여줄 데이터 줄 갯수
+//		private int pageNumToShow = Constants.PAGE_NUM_TO_SHOW;		// 화면에 보여줄 페이징 번호 갯수
+		
+		private int rowNumToShow = 10;								// 화면에 보여줄 데이터 줄 갯수
+		private int pageNumToShow = 5;								// 화면에 보여줄 페이징 번호 갯수
+		private int totalRows;										// 전체 데이터 갯수
+		private int totalPages;										// 전체 페이지 번호
+		private int startPage;										// 시작 페이지 번호
+		private int endPage;										// 마지막 페이지 번호
+		
+		private int startRnumForOracle = 1;							// 쿼리 시작 row
+		private int endRnumForOracle;								// 쿼리 끝 row
+		private Integer RNUM;
+
+		private int startRnumForMysql = 0;							// 쿼리 시작 row
+		
+		public void setParamsPaging(int totalRows) {
+			
+//			setThisPage(3);
+
+			setTotalRows(totalRows);
+
+			if (getTotalRows() == 0) {
+				setTotalPages(1);
+			} else {
+				setTotalPages(getTotalRows() / getRowNumToShow());
+			}
+
+			if (getTotalRows() % getRowNumToShow() > 0) {
+				setTotalPages(getTotalPages() + 1);
+			}
+
+			if (getTotalPages() < getThisPage()) {
+				setThisPage(getTotalPages());
+			}
+			
+			setStartPage(((getThisPage() - 1) / getPageNumToShow()) * getPageNumToShow() + 1);
+
+			setEndPage(getStartPage() + getPageNumToShow() - 1);
+
+			if (getEndPage() > getTotalPages()) {
+				setEndPage(getTotalPages());
+			}
+			
+			setEndRnumForOracle((getRowNumToShow() * getThisPage()));
+			setStartRnumForOracle((getEndRnumForOracle() - getRowNumToShow()) + 1);
+			if (getStartRnumForOracle() < 1) setStartRnumForOracle(1);
+			
+			if (thisPage == 1) {
+				setStartRnumForMysql(0);
+			} else {
+				setStartRnumForMysql((getRowNumToShow() * (getThisPage()-1)));
+			}
+			
+			System.out.println("getThisPage():" + getThisPage());
+			System.out.println("getTotalRows():" + getTotalRows());
+			System.out.println("getRowNumToShow():" + getRowNumToShow());
+			System.out.println("getTotalPages():" + getTotalPages());
+			System.out.println("getStartPage():" + getStartPage());
+			System.out.println("getEndPage():" + getEndPage());		
+			System.out.println("getStartRnumForOracle():" + getStartRnumForOracle());
+			System.out.println("getEndRnumForOracle():" + getEndRnumForOracle());
+			System.out.println("getStartRnumForMysql(): " + getStartRnumForMysql());
+			
+		}
+
+		public String getSeq() {
+			return seq;
+		}
+
+		public void setSeq(String seq) {
+			this.seq = seq;
+		}
+
+		public Integer getType() {
+			return type;
+		}
+
+		public void setType(Integer type) {
+			this.type = type;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getZipCode() {
+			return zipCode;
+		}
+
+		public void setZipCode(String zipCode) {
+			this.zipCode = zipCode;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public String getAddressDetail() {
+			return addressDetail;
+		}
+
+		public void setAddressDetail(String addressDetail) {
+			this.addressDetail = addressDetail;
+		}
+
+		public String getSido() {
+			return sido;
+		}
+
+		public void setSido(String sido) {
+			this.sido = sido;
+		}
+
+		public String getTel() {
+			return tel;
+		}
+
+		public void setTel(String tel) {
+			this.tel = tel;
+		}
+
+		public String getOpenTime() {
+			return openTime;
+		}
+
+		public void setOpenTime(String openTime) {
+			this.openTime = openTime;
+		}
+
+		public String getClosedTime() {
+			return closedTime;
+		}
+
+		public void setClosedTime(String closedTime) {
+			this.closedTime = closedTime;
+		}
+
+		public String getAdultPrice() {
+			return adultPrice;
+		}
+
+		public void setAdultPrice(String adultPrice) {
+			this.adultPrice = adultPrice;
+		}
+
+		public String getChildPrice() {
+			return childPrice;
+		}
+
+		public void setChildPrice(String childPrice) {
+			this.childPrice = childPrice;
+		}
+
+		public String getDetailText() {
+			return detailText;
+		}
+
+		public void setDetailText(String detailText) {
+			this.detailText = detailText;
+		}
+
+		public int getThisPage() {
+			return thisPage;
+		}
+
+		public void setThisPage(int thisPage) {
+			this.thisPage = thisPage;
+		}
+
+		public int getRowNumToShow() {
+			return rowNumToShow;
+		}
+
+		public void setRowNumToShow(int rowNumToShow) {
+			this.rowNumToShow = rowNumToShow;
+		}
+
+		public int getPageNumToShow() {
+			return pageNumToShow;
+		}
+
+		public void setPageNumToShow(int pageNumToShow) {
+			this.pageNumToShow = pageNumToShow;
+		}
+
+		public int getTotalRows() {
+			return totalRows;
+		}
+
+		public void setTotalRows(int totalRows) {
+			this.totalRows = totalRows;
+		}
+
+		public int getTotalPages() {
+			return totalPages;
+		}
+
+		public void setTotalPages(int totalPages) {
+			this.totalPages = totalPages;
+		}
+
+		public int getStartPage() {
+			return startPage;
+		}
+
+		public void setStartPage(int startPage) {
+			this.startPage = startPage;
+		}
+
+		public int getEndPage() {
+			return endPage;
+		}
+
+		public void setEndPage(int endPage) {
+			this.endPage = endPage;
+		}
+
+		public int getStartRnumForOracle() {
+			return startRnumForOracle;
+		}
+
+		public void setStartRnumForOracle(int startRnumForOracle) {
+			this.startRnumForOracle = startRnumForOracle;
+		}
+
+		public int getEndRnumForOracle() {
+			return endRnumForOracle;
+		}
+
+		public void setEndRnumForOracle(int endRnumForOracle) {
+			this.endRnumForOracle = endRnumForOracle;
+		}
+
+		public Integer getRNUM() {
+			return RNUM;
+		}
+
+		public void setRNUM(Integer rNUM) {
+			RNUM = rNUM;
+		}
+
+		public int getStartRnumForMysql() {
+			return startRnumForMysql;
+		}
+
+		public void setStartRnumForMysql(int startRnumForMysql) {
+			this.startRnumForMysql = startRnumForMysql;
+		}
+	
+		
+		
+		
+		
+		
+		
+}
