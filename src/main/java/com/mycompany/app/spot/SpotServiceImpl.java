9dfd6de04b724862e1451f976cfafc2364b37abb
RyuGyoungBin <132864227+RyuGyoungBin@users.noreply.github.com>
2023-08-22 14:11:07 +0900

    Update SpotServiceImpl.java

--- src/main/java/com/mycompany/app/spot/SpotServiceImpl.java
+++ src/main/java/com/mycompany/app/spot/SpotServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.mycompany.app.common.constants.Constants;
+import com.mycompany.app.common.constant.Constants;
 
 @Service
 public class SpotServiceImpl implements SpotService{
