d1d421300dc0ede5a4de144a7a385d12348142b7
songseungkwan <ssk3553@Naver.com>
2023-08-24 15:03:39 +0900

    0824 송

--- src/main/java/com/mycompany/app/infra/index/IndexController.java
+++ src/main/java/com/mycompany/app/infra/index/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.app.train.Train;
 import com.mycompany.app.train.TrainProc;
 
 @Controller
@@ -17,9 +18,9 @@ public class IndexController {
 	}
 	
 	@RequestMapping("/bookingUsrView")
-	public String bookingUsr(Model model) throws Exception {
+	public String bookingUsr(Model model , Train train) throws Exception {
 		TrainProc.train(model);
-		TrainProc.trainStation(model);
+		TrainProc.trainStation(model,train);
 		return "/usr/infra/index/bookingUsrView";
 	}
 	
