d90933aa452e079415d51e8f08a68e9a1c335bea
songseungkwan <ssk3553@Naver.com>
2023-08-30 17:29:50 +0900

    0830 모르겠어

--- src/main/java/com/mycompany/app/common/constant/Constants.java
+++ src/main/java/com/mycompany/app/common/constant/Constants.java
@@ -2,8 +2,11 @@ package com.mycompany.app.common.constant;
 
 public class Constants {
 
-	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "D://factory/ws_sts4_warkspace/prj_spring_TagMatch/src/main/webapp";
-	public static final String UPLOAD_PATH_PREFIX = "D://factory/ws_sts4_warkspace/prj_spring_TagMatch/src/main/webapp/resources/uploaded";
+	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "F:\\Factory\\prj_spring_TagMatch\\src\\main\\webapp";
+
+	
+	public static final String UPLOAD_PATH_PREFIX = "F:\\Factory\\prj_spring_TagMatch\\src\\main\\webapp\\resources\\uploaded";
+
 	public static final String UPLOAD_PATH_PREFIX_FOR_VIEW = "/resources/uploaded";
 	
 	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
